c1b05915944442a03290b9d16e22ecd11333a608
hannesi <hannesi@iki.fi>
2025-07-02 14:37:26 +0300

    naming

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -23,7 +23,7 @@ func main() {
 	gbn, err := clientprotocol.NewSelectiveRepeatProtocolClient(socket)
 
 	if err != nil {
-		panic(fmt.Sprintf("Failed to start GBN protocol client: %v", err))
+		panic(fmt.Sprintf("Failed to start SR protocol client: %v", err))
 	}
 
 	var messages [][]byte
